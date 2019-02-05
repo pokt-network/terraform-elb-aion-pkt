b66cf17ccff452e7446940b9cb4e9e8d2fb54b70
aqt01 <lowell.abbott@gmail.com>
2019-02-05 00:58:08 -0400

    using differente role per region and environment in order to differentiate from other regions

--- iam.tf
+++ iam.tf
@@ -1,6 +1,6 @@
 # iam roles
 resource "aws_iam_role" "app-ec2-role" {
-    name = "app-ec2-role-${var.environment}"
+    name = "app-ec2-role-${var.environment}-${var.aws_region}"
     assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -18,13 +18,13 @@ resource "aws_iam_role" "app-ec2-role" {
 EOF
 }
 resource "aws_iam_instance_profile" "app-ec2-role" {
-    name = "app-ec2-role-${var.environment}"
+    name = "app-ec2-role-${var.environment}-${var.aws_region}"
     roles = ["${aws_iam_role.app-ec2-role.name}"]
 }
 
 # service
 resource "aws_iam_role" "elasticbeanstalk-service-role" {
-    name = "elasticbeanstalk-service-role-${var.environment}"
+    name = "elasticbeanstalk-service-role-${var.environment}-${var.aws_region}"
     assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -44,29 +44,29 @@ EOF
 
 # policies
 resource "aws_iam_policy_attachment" "app-attach1" {
-    name = "app-attach1-${var.environment}"
+    name = "app-attach1-${var.environment}-${var.aws_region}"
     roles = ["${aws_iam_role.app-ec2-role.name}"]
     policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
 }
 resource "aws_iam_policy_attachment" "app-attach2" {
-    name = "app-attach2-${var.environment}"
+    name = "app-attach2-${var.environment}-${var.aws_region}"
     roles = ["${aws_iam_role.app-ec2-role.name}"]
     policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker"
 }
 resource "aws_iam_policy_attachment" "app-attach3" {
-    name = "app-attach3-${var.environment}"
+    name = "app-attach3-${var.environment}-${var.aws_region}"
     roles = ["${aws_iam_role.app-ec2-role.name}"]
     policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier"
 }
 resource "aws_iam_policy_attachment" "app-attach4" {
-    name = "app-attach4-${var.environment}"
+    name = "app-attach4-${var.environment}-${var.aws_region}"
     roles = ["${aws_iam_role.elasticbeanstalk-service-role.name}"]
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkEnhancedHealth"
 }
 # ecr
 # uncomment for ecr
 #resource "aws_iam_role_policy" "app-ec2-role-policy" {
-#    name = "app-ec2-role-policy-${var.environment}"
+#    name = "app-ec2-role-policy-${var.environment}-${var.aws_region}"
 #    role = "${aws_iam_role.app-ec2-role.id}"
 #    policy = <<EOF
 #{
